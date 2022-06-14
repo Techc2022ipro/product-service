47adadd61db6909dbbddcf988f2f5d282d88f148
sabi1125 <sabir_barahi4@hotmail.com>
2022-06-14 09:23:11 +0900

    start development

--- productbackend/app/src/server.ts
+++ productbackend/app/src/server.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import helmet from 'helmet';
 import Logger from './libraries/libs/Logger';
+import router from './routes/index';
 import cors from 'cors';
 const app = express();
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
-
+app.use(router);
 app.listen(2000, () => {
     Logger.info('running');
     Logger.info('@http://localhost:2000');
