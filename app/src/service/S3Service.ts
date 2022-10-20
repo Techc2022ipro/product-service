be46a71eaaa7b4a7946d160e6f08532f093fe6b9
sabi1125 <sabir_barahi4@hotmail.com>
2022-10-20 09:57:27 +0900

    refactor error handlint

--- app/src/service/S3Service.ts
+++ app/src/service/S3Service.ts
@@ -1,4 +1,5 @@
 import {S3ServiceInterface} from "@/interfaces/S3Interfaces";
+import {InternalServerError} from "@/libraries/libs/error/Errors";
 import {S3} from "aws-sdk";
 import fs from "fs";
 
@@ -9,15 +10,17 @@ export const S3Service: S3ServiceInterface= {
       accessKeyId: process.env.AWS_ACCESS_KEY,
       secretAccessKey: process.env.AWS_SECRET_KEY
     })
-    if(process.env.AWS_BUCKET) {
-      const fileStream = fs.createReadStream(image.path);
-      const uploadParams = {
-        Bucket: process.env.AWS_BUCKET,
-        Body: fileStream,
-        Key: image.filename
-       }
-      const upload = await s3.upload(uploadParams).promise().catch(e => console.log(e));
+
+    if(!process.env.AWS_BUCKET) throw new InternalServerError();
+    const fileStream = fs.createReadStream(image.path);
+    const uploadParams = {
+      Bucket: process.env.AWS_BUCKET,
+      Body: fileStream,
+      Key: image.filename
     }
-    return "image uploaded";
+
+    const upload = await s3.upload(uploadParams).promise().catch(e => console.log(e));
+    if(!upload) throw new InternalServerError();
+    return upload.Key;
   } 
 }
