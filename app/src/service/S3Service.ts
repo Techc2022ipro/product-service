576c1431a71120f2341148ed7169781a9dba652a
sabi1125 <sabir_barahi4@hotmail.com>
2022-10-21 12:04:51 +0900

    refactro s3Service

--- app/src/service/S3Service.ts
+++ app/src/service/S3Service.ts
@@ -3,14 +3,14 @@ import {InternalServerError} from "@/libraries/libs/error/Errors";
 import {S3} from "aws-sdk";
 import fs from "fs";
 
+const s3 = new S3({
+  region: process.env.AWS_REGION,
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_KEY
+})
+
 export const S3Service: S3ServiceInterface= {
   async uploadImageService(image) {
-    const s3 = new S3({
-      region: process.env.AWS_REGION,
-      accessKeyId: process.env.AWS_ACCESS_KEY,
-      secretAccessKey: process.env.AWS_SECRET_KEY
-    })
-
     if(!process.env.AWS_BUCKET) throw new InternalServerError();
     const fileStream = fs.createReadStream(image.path);
     const uploadParams = {
