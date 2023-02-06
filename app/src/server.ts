48d524303cffe40d758442a45c3ed1a3e99f8ab7
sabi1125 <sabir_barahi4@hotmail.com>
2023-02-06 21:49:17 +0900

    added: cors origin

--- app/src/server.ts
+++ app/src/server.ts
@@ -8,7 +8,7 @@ import cookieParser from 'cookie-parser';
 const app = express();
 app.use(cookieParser());
 app.use(cors({
-  origin: ["http://localhost:3000","http://localhost:4000", "https://look-book.net", "https://seller.look-book.net"],
+  origin: ["http://localhost:3000","http://localhost:4000", "https://look-book.net", "https://seller.look-book.net", "http://prod-service:2000"],
   credentials: true
 }));
 app.use(helmet());
